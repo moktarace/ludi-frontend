d82cf0ada04d345d0fba3f15b708188e33fcafe9
Ahmad <ahmad.chettih@amadeus.com>
2022-09-03 15:36:21 +0200

    Update the monthly generation design

--- src/app/services/ludi.service.ts
+++ src/app/services/ludi.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { debounceTime, delay, Observable, of } from 'rxjs';
+import { delay, Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Show } from './../model';
 import { show } from './ludi.service.mock-data';

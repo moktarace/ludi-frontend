3da39a22d4ef334d4d1b309108a958f50156df00
moktarace <lejazzman1@gmail.com>
2026-07-23 12:02:42 +0200

    feat: refactor format component for improved structure and styling

--- src/app/components/format/format.component.ts
+++ src/app/components/format/format.component.ts
@@ -1,27 +1,9 @@
-import { Component, Input } from '@angular/core'
-import { Show } from 'src/app/model'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-format',
   templateUrl: './format.component.html',
 })
 export class FormatComponent {
-
-  @Input()
-  public shows?: Show[] | null = [];
-
   constructor() { }
-
-  public get capShows(): Show[] {
-    return this.shows?.filter(s => s.location?.toLowerCase().includes("cap")) || [];
-  }
-
-
-  public get improvisemShows(): Show[] {
-    return this.shows?.filter(s => s.name?.toLowerCase().includes("improvisem")) || [];
-  }
-
-  public get catchShows(): Show[] {
-    return this.shows?.filter(s => s.name?.toLowerCase().includes("catch")) || [];
-  }
 }
